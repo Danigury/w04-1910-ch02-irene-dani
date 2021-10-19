d514134531736a3cb2d09efc451601d8c22b80cb
ipice1618 <72157834+ipice1618@users.noreply.github.com>
2021-10-19 20:38:53 +0200

    created an array of objects of letters

--- src/App.js
+++ src/App.js
@@ -2,7 +2,49 @@ import "./App.css";
 import LetterToClick from "./componentes/LetterToClick/LetterToClick";
 
 function App() {
-  return ();
+  const letters = [
+    "A",
+    "B",
+    "C",
+    "D",
+    "E",
+    "F",
+    "G",
+    "H",
+    "I",
+    "J",
+    "K",
+    "L",
+    "M",
+    "N",
+    "Ñ",
+    "O",
+    "P",
+    "Q",
+    "R",
+    "S",
+    "T",
+    "U",
+    "V",
+    "W",
+    "X",
+    "Y",
+    "Z",
+  ];
+  const letterObjects = [];
+  const word = "CASA";
+
+  for (i of letters) {
+    letterObjects.push({
+      id: 1,
+      letter: i,
+      hasBeenClicked: false,
+      isInWord: word.includes(i) ? true : false,
+    });
+  }
+
+  console.log(letterObjects);
+  return;
 }
 
 export default App;
