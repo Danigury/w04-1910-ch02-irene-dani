57dc8d7ca57525a14caa3f28b05f00428df96d0c
ipice1618 <72157834+ipice1618@users.noreply.github.com>
2021-10-19 20:17:31 +0200

    LetterToClick component works

--- src/componentes/LetterTOClick/LetterToClick.js
+++ src/componentes/LetterTOClick/LetterToClick.js
@@ -2,7 +2,7 @@ import PropTypes from "prop-types";
 
 function LetterToClick({ letter, functionOnClick, className }) {
   return (
-    <div className={className} onClick={functionOnClick}>
+    <div className={"letter-to-click " + className} onClick={functionOnClick}>
       {letter}
     </div>
   );
