487cfd46edfa2afe709ca3c95854a212bde59f75
Dani Sanchez <danysq87@gmail.com>
2021-10-19 20:36:38 +0200

    Created LetterToGuess & functionality

---
+++ src/componentes/letter-to-guess/LetterToGuess.js
@@ -0,0 +1,9 @@
+const LetterToGuess = ({ letterToGuess }) => {
+  // const letter = { letter };
+  // const wordToDash = word.split("").fill("_").join(" ");
+  // console.log(wordToDash);
+
+  return <div className="letter-to-guess">{letterToGuess}</div>;
+};
+
+export default LetterToGuess;
