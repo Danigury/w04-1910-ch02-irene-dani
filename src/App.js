e0e6e9bbfec624116df7cc6740acc74692527360
Dani Sanchez <danysq87@gmail.com>
2021-10-19 22:40:38 +0200

    Added bug fixes of eslint, now wordToGuess is working

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
+import { useState } from "react";
 import "./App.css";
+import LetterToGuess from "./componentes/letter-to-guess/LetterToGuess";
 import LetterToClick from "./componentes/LetterToClick/LetterToClick";
 
 function App() {
@@ -31,35 +33,49 @@ function App() {
     "Y",
     "Z",
   ];
-  const letterArray = [];
-  const word = "CASA";
 
-  for (let i of letters) {
+  const letterArray = [];
+  const words = ["PINYA", "AMB", "PIZZA", "TREGUA", "TACTICA"];
+  // const word = parseInt(Math.floor(Math.random() * words.length));
+  const word = words[0];
+  console.log(word);
+  const emptyArray = new Array(word.length).fill("_");
+  console.log(emptyArray);
+  for (let i = 0; i < letters.length; i++) {
     letterArray.push({
-      id: 1,
-      letter: i,
-      isInWord: word.includes(i) ? true : false,
+      id: i,
+      letter: letters[i],
+      isInWord: word.includes(letters[i]) ? true : false,
     });
   }
 
-  /*  event.target.contains.className("");  */
-  function change() {
-    console.log("hola");
+  const [wordUpdate, setWordUpdate] = useState(emptyArray);
+  function change(sign) {
+    for (let i in word) if (word[i] === sign) wordUpdate[i] = sign;
+    setWordUpdate(wordUpdate);
   }
 
-  const words = ["PINYA", "AMB", "PIZZA", "TREGUA", "TACTICA"];
-  const randomWord = Math.floor(Math.random() * words.length);
-
   return (
-    <section>
-      {letterArray.map((letterObject) => (
-        <LetterToClick
-          letter={letterObject.letter}
-          isInWord={letterObject.isInWord}
-          actionOnClick={change}
-        />
-      ))}
-    </section>
+    <>
+      <section className="letters-to-click">
+        {letterArray.map((letterObject) => (
+          <LetterToClick
+            key={letterObject.id}
+            letter={letterObject.letter}
+            isInWord={letterObject.isInWord}
+            actionOnClick={() => {
+              change(letterObject.letter);
+            }}
+          />
+        ))}
+      </section>
+      <LetterToGuess letterToGuess={"a"} />
+      <section className="letters-to-guess">
+        {wordUpdate.map((letter, index) => (
+          <LetterToGuess letterToGuess={letter} key={index} />
+        ))}
+      </section>
+    </>
   );
 }
 
